49eee8e5076e913eb595f1d966f7e6ea00fc4942
ritikraj018 <78099566+ritikraj018@users.noreply.github.com>
2023-02-04 14:11:07 +0530

    Create fancypattern.cpp

---
+++ fancypattern.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n=5;
+
+    for(int row=0; row<n; row++){
+
+      for(int col=0; col < n-row-1; col++){
+        cout<<" ";
+      }
+      int start =1;
+      for( int col =0; col < 2*row+1; col++)
+        {
+          if(row == 0 || row == n-1){
+            if(col %2==0){
+              cout<<start++;
+            }
+            else{
+              cout<<" ";
+            }
+          }
+          else{
+              if(col ==0){
+                cout<<1;
+              }
+              else if( col == 2 * row){
+                cout<<row+1;
+              }
+              else{
+                cout<<" ";
+              }
+          }
+        }
+      cout<<endl;
+    }
+}
